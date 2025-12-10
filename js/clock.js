da7a208a6b1533198d76a45e9d6943cc44b3327c
H-24 <pyj0318jgmw@gmail.com>
2025-12-11 01:25:49 +0900

    clock.js 수정

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,9 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const date = new Date();
-    hour = String(date.getHours()).padStart(2, "0");
-    min = String(date.getMinutes()).padStart(2, "0");
-    sec = String(date.getSeconds()).padStart(2, "0");
+    const hour = String(date.getHours()).padStart(2, "0");
+    const min = String(date.getMinutes()).padStart(2, "0");
+    const sec = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hour}:${min}:${sec}`;
 }
 
