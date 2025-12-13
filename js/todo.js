19419a54972dd840f8cc089b3104e66c03f0bbf1
H-24 <pyj0318jgmw@gmail.com>
2025-12-14 05:29:00 +0900

    toDo List-2/ 리스트 생성하고 삭제하기

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,36 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+
+function saveTodo() {
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
+function deleteTodo(e) {
+    const li = e.target.parentElement;
+    li.remove();
+}
+
+function CreateTodo(text) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
 function ToDoSubmit(e) {
     e.preventDefault();
     const toDo = toDoInput.value;
-    console.log(toDo);
     toDoInput.value = ""
+    toDos.push(toDo);
+    CreateTodo(toDo);
+    saveTodo();
 }
 
 toDoForm.addEventListener("submit", ToDoSubmit);
